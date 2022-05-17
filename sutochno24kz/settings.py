06ef219a1a02c638e0fc169c0ec1c8fa326fa88c
root <root@server.sutochno24.kz>
2022-05-17 09:35:44 +0200

    settings for server

--- sutochno24kz/settings.py
+++ sutochno24kz/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ilsl^c1eqtw!#+1341^yf-)z!ck8ibbtp4@)fjkrph_p-=ccu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
