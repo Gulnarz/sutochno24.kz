d2ac731d4cc588b8f44985ccb9eba44ec3214cba
Timur <nbveh211001@gmail.com>
2022-07-04 11:14:45 +0600

    frontend changes

--- sutochno24kz/settings.py
+++ sutochno24kz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ilsl^c1eqtw!#+1341^yf-)z!ck8ibbtp4@)fjkrph_p-=ccu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
