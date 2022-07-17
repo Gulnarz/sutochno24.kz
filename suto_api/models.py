bff50147662e80e1b834dc0dfa412d0c0d64b213
Timur <nbveh211001@gmail.com>
2022-07-17 21:32:19 +0600

    Photoes 12 and carousel

--- suto_api/models.py
+++ suto_api/models.py
@@ -7,6 +7,11 @@ types = [
     ("Д", "Двухкомнатная")
 ]
 
+statuses = [
+    ("С", "Свободный"),
+    ("З", "Занят")
+]
+
 class Apartment(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=250)
@@ -14,12 +19,19 @@ class Apartment(models.Model):
     max_person = models.CharField(max_length=50)
     price = models.IntegerField()
     description = models.TextField()
-    photo_main = models.FileField(upload_to="apartments", max_length=100)
-    photo_main_little = models.FileField(upload_to="apartments", max_length=100)
-    photo_1 = models.FileField(upload_to="apartments", max_length=100)
-    photo_2 = models.FileField(upload_to="apartments", max_length=100)
-    photo_3 = models.FileField(upload_to="apartments", max_length=100)
-    photo_4 = models.FileField(upload_to="apartments", max_length=100)
-    photo_5 = models.FileField(upload_to="apartments", max_length=100)
-    photo_6 = models.FileField(upload_to="apartments", max_length=100)
+    status = models.CharField(max_length=1, choices=statuses, default="C")
+    photo_main = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_main_little = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_1 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_2 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_3 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_4 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_5 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_6 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_7 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_8 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_9 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_10 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_11 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
+    photo_12 = models.FileField(upload_to="apartments", max_length=100, null=True, blank=True)
     googlemap_addres = models.CharField(max_length=10000)
